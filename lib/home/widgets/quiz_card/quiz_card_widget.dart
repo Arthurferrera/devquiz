fc12010c8b0a2d55e214ed00d59c7d7ee0d97756
Arthur Ferreira <arthur_f.oliveira@hotmail.com>
2021-04-25 00:54:30 -0300

    create listview of quiz card

--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -7,6 +7,7 @@ class QuizCardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(16),
       decoration: BoxDecoration(
         color: AppColors.white,
         borderRadius: BorderRadius.circular(10),
@@ -17,22 +18,29 @@ class QuizCardWidget extends StatelessWidget {
         ),
       ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
             height: 40,
             width: 40,
             child: Image.asset(AppImages.blocks),
           ),
+          SizedBox(
+            height: 20,
+          ),
           Text(
             "Gerenciamento de Estado",
             style: AppTextStyles.heading15,
+          ),
+          SizedBox(
+            height: 20,
           ),
           Row(
             children: [
               Expanded(
                 flex: 1,
                 child: Text(
-                  "3 de 10",
+                  "3/10",
                   style: AppTextStyles.body11,
                 ),
               ),
