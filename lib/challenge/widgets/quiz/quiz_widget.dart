2cd47f8bc9d572f3749a0d337d184eda4fde033e
Arthur Ferreira <arthur_f.oliveira@hotmail.com>
2021-04-25 21:43:50 -0300

    create answer and condition selected

--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,3 +1,4 @@
+import 'package:devquiz/challenge/widgets/awnser/awnser_widget.dart';
 import 'package:devquiz/core/core.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +17,18 @@ class QuizWidget extends StatelessWidget {
           ),
           SizedBox(
             height: 24,
-          )
+          ),
+          AwnserWidget(
+              isRight: true,
+              isSelected: true,
+              title:
+                  "Possibilita a criação de aplicativos compilados nativamente. "),
+          AwnserWidget(
+              title:
+                  "Possibilita a criação de aplicativos compilados nativamente. "),
+          AwnserWidget(
+              title:
+                  "Possibilita a criação de aplicativos compilados nativamente. ")
         ],
       ),
     );
